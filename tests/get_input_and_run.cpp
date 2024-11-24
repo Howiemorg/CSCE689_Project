322bac396d41edebb58b2ca981f3d8a050708076
Howiemorg <morgenthalerhowie@yahoo.com>
2024-11-24 01:59:00 -0600

    Fixed cpp test

--- tests/get_input_and_run.cpp
+++ tests/get_input_and_run.cpp
@@ -50,13 +50,29 @@ void printVector(vector<T> vec){
     int n = vec.size() - 1; cout << "["; 
     for(int i = 0; i <= n; ++i){ 
         // cout << i << " N: " << n << "\n"; 
-        cout << vec[i] << (i == n ? ']' : ','); 
+        cout << vec[i] << (i == n ? "]\n" : ", "); 
         } 
     }
 vector<int> twoSum(vector<int> nums, int target){ 
     printVector(nums); 
-    cout << target << "\n"; return nums; 
+    cout << target << "\n"; 
+    return nums; 
 } 
 int main() { 
     string input; 
-    int pos = 1; int prevPos = 1; vector<int> nums; cin >> input; cout << input << "\n"; pos = 1; prevPos = 1; while ((pos = input.find(',')) != string::npos){ nums.push_back(stoi(input.substr(prevPos, pos-prevPos))); prevPos = pos + 1; } nums.push_back(stoi(input.substr(prevPos, input.length() - 1 - prevPos))); cin >> input; int target = stoi(input); printVector(twoSum(nums,target)); }
+    int pos = 1; 
+    int prevPos = 1; 
+    vector<int> nums; 
+    cin >> input; 
+
+    pos = 1; 
+    prevPos = 1; 
+    while ((pos = input.find(',', prevPos)) != string::npos){ 
+        nums.push_back(stoi(input.substr(prevPos, pos-prevPos))); 
+        prevPos = pos + 1; 
+    }
+    nums.push_back(stoi(input.substr(prevPos, input.length() - 1 - prevPos))); 
+    cin >> input;
+
+    int target = stoi(input); 
+    printVector(twoSum(nums,target)); }
